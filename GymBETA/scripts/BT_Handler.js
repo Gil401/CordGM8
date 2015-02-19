d267c2bac88c7a1fc0ef2ecc7d87e50521d7edf7
Gil401 <Gil401@gmail.com>
2015-02-19 17:14:22 +0200

    First Commit

---
+++ GymBETA/scripts/BT_Handler.js
@@ -0,0 +1,51 @@
+﻿function checkBluetoothStatus() {
+    var btstatus = document.getElementById('status');
+    btstatus.innerHTML = "Checking bluetooth information";
+    if (isBTEnabled) {
+        btstatus.innerHTML = "Bluetooth is enabled";
+    } else {
+        btstatus.innerHTML = "Bluetooth is disabled";
+    }
+    //window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
+}
+
+function isEnabledSuccess(isEnabled) {
+    var btstatus = document.getElementById('status');
+    if (isEnabled) {
+        btstatus.innerHTML = "Enabled";
+    } else {
+        btstatus.innerHTML = "Disabled";
+    }
+}
+
+function isEnabledError(error) {
+    var btstatus = document.getElementById('status');
+    btstatus.innerHTML = "Cannot determine Bluetooth status: " + error.message;
+}
+
+function enableBluetooth() {
+    var btstatus = document.getElementById('status');
+    btstatus.innerHTML = "Attempting to turn bluetooth on";
+    //window.bluetooth.enable(function () { alert("good") }, function () { alert("bad") });
+}
+
+function isBTEnabled() {
+    bluetoothSerial.
+    bluetoothSerial.isEnabled(
+        function() {
+            alert("Bluetooth is enabled");
+            return true;
+        },
+        function() {
+            alert("Bluetooth is *not* enabled");
+            return false;
+        }
+)}
+
+function BTconnected() {
+    alert("good");
+}
+
+function BTfailed() {
+    alert("bad");
+}
---
+++ GymBETA/scripts/NFC_Handler.js
@@ -0,0 +1,21 @@
+﻿function initNFC() {
+    nfc.addNdefListener(
+            function (nfcEvent) {
+                var tag = nfcEvent.tag,
+                    ndefMessage = tag.ndefMessage;
+                // assuming the first record in the message has 
+                // a payload that can be converted to a string.
+                var macAddress = nfc.bytesToString(ndefMessage[0].payload).substring(3);
+                if (confirm(macAddress)) {
+                    //00:1b:b1:fc:d3:70
+                    bluetoothSerial.connect("00:1b:b1:fc:d3:70", BTconnected, BTfailed);
+                }
+            },
+            function () { // success callback
+                console.log("Waiting for NDEF tag");
+            },
+            function (error) { // error callback
+                console.log("Error adding NDEF listener " + JSON.stringify(error));
+            }
+        );
+}
